c9393c6f9dec1d631adf168a239ab31c981ad19f
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-01-30 01:43:21 +0600

    6. Routing, code splitting, lazy, suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import {Counter} from './components/Counter'
+import {Suspense} from 'react'
+import {Link, Route, Routes} from 'react-router-dom'
 import './index.scss'
+import {AboutPageLazy} from './pages/AboutPage/AboutPage.lazy'
+import {MainPageLazy} from './pages/MainPage/MainPage.lazy'
 
 export const App = () => {
 	return (
 		<div className='app'>
-			fdasfadsfdas
-			<Counter/>
+			<div>
+				<Link to='/'>Main Page</Link>
+			</div>
+			<div>
+				<Link to='/about'>About Page</Link>
+			</div>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path='/about' element={<AboutPageLazy/>}/>
+					<Route path='/' element={<MainPageLazy/>}/>
+				</Routes>
+			</Suspense>
 		</div>
 	)
 }
