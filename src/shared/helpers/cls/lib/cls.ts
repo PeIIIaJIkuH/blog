8a2c6a61e19fc76eeed7da880eedc327532839ea
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-02-16 13:54:47 +0600

    Updated cls helper

--- src/shared/helpers/cls/lib/cls.ts
+++ src/shared/helpers/cls/lib/cls.ts
@@ -1,24 +1,31 @@
 type Mod = string | boolean | number | null | undefined | Record<string | number, boolean | number | null | undefined>
 
+const validate = (mod: Mod): string => {
+	if (typeof mod === 'string') {
+		return mod
+	}
+	if (typeof mod === 'number') {
+		return mod ? String(mod) : ''
+	}
+	if (typeof mod === 'boolean') {
+		return ''
+	}
+	if (mod === null || mod === undefined) {
+		return ''
+	}
+	return Object.keys(mod)
+		.filter((key) => mod[key])
+		.join(' ')
+}
+
 export const cls = (...mods: Mod[]): string =>
 	mods
-		.map((mod) => {
-			if (typeof mod === 'string') {
-				return mod
+		.reduce((acc: string[], mod) => {
+			const modString = validate(mod)
+			if (modString) {
+				acc.push(modString)
 			}
-			if (typeof mod === 'number') {
-				return mod ? String(mod) : ''
-			}
-			if (typeof mod === 'boolean') {
-				return ''
-			}
-			if (mod === null || mod === undefined) {
-				return ''
-			}
-			return Object.keys(mod)
-				.filter((key) => mod[key])
-				.join(' ')
-		})
-		.filter(Boolean)
+			return acc
+		}, [])
 		.join(' ')
 		.trim()
