7b41f9fb8c1142e265198a0bddad1047ace81d47
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-02-21 15:02:20 +0600

    30

--- src/shared/helpers/render-component/index.tsx
+++ src/shared/helpers/render-component/index.tsx
@@ -3,6 +3,8 @@ import { type ReactNode } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { MemoryRouter } from 'react-router-dom'
 
+// eslint-disable-next-line boundaries/element-types
+import { StoreProvider } from 'app/providers/store'
 import i18nForTests from 'shared/config/i18n-for-tests'
 
 interface RenderComponentOptions {
@@ -13,8 +15,10 @@ export const renderComponent = (component: ReactNode, options: RenderComponentOp
 	const { route = '/' } = options
 
 	return render(
-		<MemoryRouter initialEntries={[route]}>
-			<I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-		</MemoryRouter>,
+		<StoreProvider>
+			<MemoryRouter initialEntries={[route]}>
+				<I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+			</MemoryRouter>
+		</StoreProvider>,
 	)
 }
