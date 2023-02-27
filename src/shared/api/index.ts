37c8764c8ae08f290497b504584f08cecc864c03
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-02-28 03:29:42 +0600

    API_URL fix

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -3,7 +3,8 @@ import axios from 'axios'
 import { LS_KEYS } from 'shared/constants/local-storage'
 
 export const api = axios.create({
-	baseURL: API_URL,
+	baseURL: 'http://localhost:8000',
+	// TODO: Replace with global variable API_URL
 	headers: {
 		Authorization: localStorage.getItem(LS_KEYS.USER),
 	},
