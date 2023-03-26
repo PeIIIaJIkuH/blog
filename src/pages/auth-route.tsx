1620863cfaf3172ce3b25a70df8458912c06bd7b
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-03-27 04:26:59 +0600

    48

--- src/pages/auth-route.tsx
+++ src/pages/auth-route.tsx
@@ -1,11 +1,11 @@
-import { type FC, type PropsWithChildren } from 'react'
+import { type FC, memo, type PropsWithChildren } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 
 import { useAppSelector } from 'app/store'
 import { getUser } from 'entities/user'
 import { RoutePath } from 'shared/config/routes'
 
-export const AuthRoute: FC<PropsWithChildren> = ({ children }) => {
+export const AuthRoute: FC<PropsWithChildren> = memo(({ children }) => {
 	const user = useAppSelector(getUser)
 	const location = useLocation()
 
@@ -14,4 +14,4 @@ export const AuthRoute: FC<PropsWithChildren> = ({ children }) => {
 	}
 
 	return <>{children}</>
-}
+})
