4cd3af559292283d6c7c95b5c3c40da55cbd7877
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-09-14 06:22:06 +0600

    fix AuthRoute component

--- src/pages/auth-route.tsx
+++ src/pages/auth-route.tsx
@@ -1,16 +1,15 @@
 import { type FC, memo, type PropsWithChildren } from 'react'
-import { Navigate, useLocation } from 'react-router-dom'
 
 import { useAppSelector } from 'app/store'
 import { getUser } from 'entities/user'
-import { RoutePath } from 'shared/config/routes'
+
+import { NotFound } from './not-found'
 
 export const AuthRoute: FC<PropsWithChildren> = memo(({ children }) => {
 	const user = useAppSelector(getUser)
-	const location = useLocation()
 
 	if (!user) {
-		return <Navigate to={RoutePath.home} state={{ from: location }} replace />
+		return <NotFound />
 	}
 
 	return <>{children}</>
