a23c0c9145e78984b4316609ef3cda2f94f8d2ae
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-09-25 05:02:06 +0600

    add tests for user-slice

---
+++ src/entities/user/model/__tests__/user-slice.test.ts
@@ -0,0 +1,15 @@
+import { type User } from '../types'
+import { userSlice, userActions, userReducer } from '../user-slice'
+
+describe('entities/user/model/user-slice', () => {
+	it('should return the initial state', () => {
+		expect(userSlice.reducer(undefined, {} as any)).toEqual({
+			user: null,
+		})
+	})
+
+	it('should handle setUser', () => {
+		const user: User = { id: 'test-id', username: 'test-username', role: 'user' }
+		expect(userReducer(undefined, userActions.setUser(user)).user).toEqual(user)
+	})
+})
