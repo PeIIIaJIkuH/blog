fad18d9327a15e6634cc678253083a8baad0cc9d
Yerkhan Baigal <baigal.erhan@gmail.com>
2023-09-26 05:58:59 +0600

    fix styles on articles page

--- src/pages/articles/ui/articles.tsx
+++ src/pages/articles/ui/articles.tsx
@@ -2,6 +2,12 @@ import { type FC, memo } from 'react'
 
 import { ViewArticles } from 'features/view-articles'
 
+import s from './articles.module.scss'
+
 export const Articles: FC = memo(() => {
-	return <ViewArticles />
+	return (
+		<div className={s.articles}>
+			<ViewArticles />
+		</div>
+	)
 })
