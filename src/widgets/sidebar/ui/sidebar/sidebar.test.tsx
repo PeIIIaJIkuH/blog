c41ed2e1b3e450a6c0b9f8439d1e3f6bc748cf6f
Yerkhan Baigal <baigal.erhan@gmail.com>
2024-04-28 06:19:45 +0500

    fix: sidebar unit tests fix

--- src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
@@ -4,6 +4,20 @@ import { renderComponent } from 'shared/helpers/render-component'
 
 import { Sidebar } from './sidebar'
 
+Object.defineProperty(window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(),
+		removeListener: jest.fn(),
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	})),
+})
+
 describe('widgets/sidebar', () => {
 	it('should render successfully', () => {
 		renderComponent(<Sidebar />)
